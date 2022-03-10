9c8423283b2bd7496bf6d0f1facf3a899047b441
PrantaDas <prantodas043@gmail.com>
2022-03-11 05:01:04 +0600

    comment fixed

--- js/app.js
+++ js/app.js
@@ -120,9 +120,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
