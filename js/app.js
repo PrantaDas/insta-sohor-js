54cdba3d32878f54ffdfa4bd50fb087ac9054b0f
PrantaDas <prantodas043@gmail.com>
2022-03-11 06:59:06 +0600

    style fixed

--- js/app.js
+++ js/app.js
@@ -42,17 +42,19 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "answer-question").style.display = "block";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "answer-question").style.display = "none";
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
-
+        document.getElementById( "answer-question").style.display = "none";
         displayReportedPosts();
     }
 };
